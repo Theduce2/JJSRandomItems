1d1126f06c56d64e321099ad5a33b678a5e26924
jsanto <jsanto@psych-0574.home>
2015-01-29 21:24:13 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by jsanto on 1/29/15.
+//  Copyright (c) 2015 Jason Santo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
